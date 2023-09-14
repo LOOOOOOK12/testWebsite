c31240b154af261397f9f90b82e8e4c964ef21a3
Luke Symon <lukealamil12@gmail.com>
2023-09-14 18:51:51 +0800

    update

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,5 +1,6 @@
 import "../Styles/NavBar.css";
 
+
 function NavBar() {
   return (
     <div className="navBar">
@@ -7,7 +8,12 @@ function NavBar() {
             <h1>LOOOOOOK</h1>
         </div>
         <div className='Links'>
-
+          <ul>
+            <li>Home</li>
+            <li>About Us</li>
+            <li>Menu</li>
+            <li>Contact</li>
+          </ul>
         </div>
     </div>
   )

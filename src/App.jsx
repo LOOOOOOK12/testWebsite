fd68e4ba119c8938b1d30cab2faf0cecf5a176da
Luke Symon <lukealamil12@gmail.com>
2023-09-14 18:28:12 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import Home from './Pages/Home';
+import NavBar from './Components/NavBar';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+
+
+function App() {
+  return (
+    <div className='App'>
+      ppppppp
+      <NavBar/>
+      <Home/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Components/NavBar.jsx
@@ -0,0 +1,16 @@
+import "../Styles/NavBar.css";
+
+function NavBar() {
+  return (
+    <div>
+        <div className='Logo'>
+            <h1>LOOOOOOK</h1>
+        </div>
+        <div className='Links'>
+
+        </div>
+    </div>
+  )
+}
+
+export default NavBar

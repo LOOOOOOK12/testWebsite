09898d256349f1ac3314ffc66ffe77c909921f46
Luke Symon <lukealamil12@gmail.com>
2023-09-19 10:19:35 +0800

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import "./App.css";
 import Home from './Pages/Home';
 import About from "./Pages/About";
+import Contacts from "./Pages/Contacts";
+import Services from "./Pages/Services";
 import NavBar from './Components/NavBar';
 
 function App() {
@@ -9,6 +11,8 @@ function App() {
       <NavBar/>
       <Home/>
       <About/>
+      <Contacts/>
+      <Services/>
     </div>
   )
 }

7b1b9f8d147a627e8122094d09ec957cb18fead9
Luke Symon <lukealamil12@gmail.com>
2023-09-20 21:39:24 +0800

    update

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,18 +1,18 @@
+import React from 'react';
 import "../Styles/NavBar.css";
-
 
 function NavBar() {
   return (
     <div className="NavBar">
-          <div className='Logo'>
-              <h1>LOOOOOOK</h1>
-          </div>
-            <ul className='Links'>
-              <li>Home</li>
-              <li>About Us</li>
-              <li>Services</li>
-              <li>Contact</li>
-            </ul> 
+      <div className='Logo'>
+          <h1>LOOOOOOK</h1>
+      </div>
+        <ul className='Links'>
+          <a href=""><li>Home</li></a>
+          <a href=""><li>About us</li></a>
+          <a href=""><li>Services</li></a>
+          <a href=""><li>Contacts</li></a>
+        </ul> 
     </div>
   )
 }

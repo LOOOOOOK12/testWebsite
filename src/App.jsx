cf65018d05317252410d14e18a1c2c72e23ad4ec
Luke Symon <lukealamil12@gmail.com>
2023-09-20 22:04:14 +0800

    update

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,16 @@ import About from "./Pages/About";
 import Contacts from "./Pages/Contacts";
 import Services from "./Pages/Services";
 import NavBar from './Components/NavBar';
-//import { BrowserRouter as Router, Routes, Route } from "react-router-dom";  
+
 
 function App() {
   return (
     <div className='App'>
       <NavBar/>
       <Home/>
-      <About/>
-      <Contacts/>
+      <About/> 
       <Services/>
+      <Contacts/>
     </div>
   )
 }

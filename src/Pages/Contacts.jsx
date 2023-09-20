3d1664bad57fb52ebfd5c63398a4f4e5af1a7509
Luke Symon <lukealamil12@gmail.com>
2023-09-20 22:06:25 +0800

    update

--- src/Pages/Contacts.jsx
+++ src/Pages/Contacts.jsx
@@ -2,7 +2,7 @@ import "../Styles/Contacts.css";
 
 function Contacts() {
   return (
-    <div className="Contacts">
+    <div className="Contacts" id="Contacts">
       <h1>Contacts</h1>
     </div>
   )

cf8d717f5bb9fb4beb8f599fb2f2ef652f06565e
Luke Symon <lukealamil12@gmail.com>
2023-09-22 20:46:47 +0800

    update

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import "../Styles/Home.css";
+import BG from "../assets/pexels-arkkrapol-anantachote-1571746 (1).jpg";
 
 
 function Home() {
   return (
-    <div className="Home" id='Home'>
-        <h1>HELLO</h1>
+    <div className ="Home" id='Home'> 
+      <h1>HELLO</h1>      
     </div>
   )
 }
